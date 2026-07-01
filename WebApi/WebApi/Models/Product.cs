bfd739e309414f3689831762ecdd76994ba4c08e
FaresMaady <farsmaady@gmail.com>
2026-07-01 15:23:08 +0300

    feat(session-02): created Product.cs

---
+++ WebApi/WebApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models;
+
+public class Product
+{
+    private string Id;
+    private string Name;
+    private string SKU;
+    private string Description;
+    private decimal Price;
+    private int QuantityInStock;
+    private string supplierName;
+    private DateTime ExpiryDate;
+    private bool IsArchived;
+    private DateTime CreatedAt;
+    private DateTime LastUpdatedAt;
+}
