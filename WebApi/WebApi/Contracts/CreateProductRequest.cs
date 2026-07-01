aaec468ee6675159e4159bd26caf4d150fe025d8
FaresMaady <farsmaady@gmail.com>
2026-07-01 15:57:53 +0300

    feat(session-02): created CreateProductRequest.cs

---
+++ WebApi/WebApi/Contracts/CreateProductRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Contracts;
+
+public class CreateProductRequest
+{
+    public string Name;
+    public string SKU;
+    public string Description;
+    public decimal Price;
+    public int QuantityInStock;
+    public string SupplierName;
+    public DateTime ExpiryDate;
+}
