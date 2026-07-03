d6a4c844b8b55d2dcfe1d4ad867c7ac8718ede1c
FaresMaady <farsmaady@gmail.com>
2026-07-03 11:02:47 +0300

    feat(session-02): implimented api endpoint 1

---
+++ WebApi/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.DB;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
+{
+    // endpoint 1 get all products (GET/api/products)
+    [HttpGet]
+    public IActionResult GetAll([FromQuery] bool onlyAvailable = false)
+    {
+        var products = FakeWarehouseStore.Products.AsEnumerable();
+        if (onlyAvailable)
+            products = products.Where(p => p.QuantityInStock > 0 && !p.IsArchived);
+
+        products = products.OrderByDescending(p => p.CreatedAt);
+
+        return Ok(products.ToList());
+    }
+}
