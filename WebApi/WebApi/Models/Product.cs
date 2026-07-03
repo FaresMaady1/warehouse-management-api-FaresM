b09e4c88ff8531f1dab382331eb4b18f79aae48e
FaresMaady <farsmaady@gmail.com>
2026-07-03 16:09:42 +0300

    feat(session-02): added supplier id to Product.cs to make the relation between them

--- WebApi/WebApi/Models/Product.cs
+++ WebApi/WebApi/Models/Product.cs
@@ -9,6 +9,7 @@ public class Product
     public decimal Price { get; set; }
     public int QuantityInStock { get; set; }
     public string SupplierName { get; set; }
+    public string SupplierId { get; set; }
     public DateTime ExpiryDate { get; set; }
     public bool IsArchived { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
