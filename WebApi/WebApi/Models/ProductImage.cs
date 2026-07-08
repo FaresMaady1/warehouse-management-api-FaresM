4f7fbe10ac6a602439778f8bb31339867897ad02
FaresMaady <farsmaady@gmail.com>
2026-07-08 09:08:08 +0300

    feat(session-03): add DomainException for business rule violations

--- WebApi/WebApi/Models/ProductImage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Models;
-
-public class ProductImage
-{
-    private string ProductId { get; set; }
-    private string FileName { get; set; }
-    private string FilePath { get; set; }
-}
