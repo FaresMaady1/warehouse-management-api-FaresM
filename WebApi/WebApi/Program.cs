3be49b0ccb1257b6d5336a6e66e71bd93d9effe7
FaresMaady <farsmaady@gmail.com>
2026-07-08 10:00:17 +0300

    refactor(session-03): register MediatR and repositories in Program.cs

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,17 +1,30 @@
-﻿using WebApi.Services;
+using MediatR;
+using Warehouse.Application.Commands.Products;
+using Warehouse.Domain.Products;
+using Warehouse.Domain.Suppliers;
+using Warehouse.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<ISupplierService, SupplierService>();
+
+builder.Services.AddSingleton<WarehouseDbContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapControllers();
 
+app.MapControllers();
 
 app.Run();
