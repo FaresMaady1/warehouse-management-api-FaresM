a968c7119bc9bce46a338c63bb026c8237218129
FaresMaady <farsmaady@gmail.com>
2026-07-11 11:17:27 +0300

    feat(session-04): created image repo

---
+++ WebApi/Warehouse.Domain/Repositories/IProductImageRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Warehouse.Domain.Repositories;
+
+using Warehouse.Domain.ProductImages;
+
+public interface IProductImageRepository
+{
+    ProductImage? GetByProductId(string productId);
+    void Add(ProductImage image);
+    void SaveChanges();
+}
