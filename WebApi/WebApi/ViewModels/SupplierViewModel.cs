7daecabad925b6b5f5c95b401f6d968b4ad3c0cb
FaresMaady <farsmaady@gmail.com>
2026-07-11 11:44:47 +0300

    feat(session-04): added view models and mapper

---
+++ WebApi/WebApi/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.ViewModels;
+
+public class SupplierViewModel
+{
+    public string Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public bool IsActive { get; set; }
+}
