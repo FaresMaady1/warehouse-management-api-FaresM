cf8af40546901e38113e1c67335d630cb3a85df5
FaresMaady <farsmaady@gmail.com>
2026-07-11 11:18:12 +0300

    feat(session-04): injected db context and added automapper

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,18 +1,19 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Warehouse.Application.Commands.CreateProduct;
 using Warehouse.Domain.Repositories;
 using Warehouse.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<WarehouseDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb")));
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<WarehouseDbContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
-
+builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
-
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
