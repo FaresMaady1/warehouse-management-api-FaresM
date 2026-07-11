ef918abea0cb418e2c7bb1b69d78ac0e16fdc776
FaresMaady <farsmaady@gmail.com>
2026-07-11 11:25:51 +0300

    fix(session-04): fixed automapper function call

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -5,15 +5,19 @@ using Warehouse.Domain.Repositories;
 using Warehouse.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<WarehouseDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb")));
-builder.Services.AddControllers();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
 builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(cfg => { }, typeof(Program).Assembly);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
