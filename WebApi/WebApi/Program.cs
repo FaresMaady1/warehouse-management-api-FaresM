310c77b7fa4c04fb13eee2c98eecf78705666a83
FaresMaady <farsmaady@gmail.com>
2026-07-15 11:53:04 +0300

    feat(session-05): injected all new features in Program.cs

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,12 +1,19 @@
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Warehouse.Application.Commands.CreateProduct;
 using Warehouse.Domain.Repositories;
 using Warehouse.Infrastructure.Persistence;
+using WebApi.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 builder.Services.AddDbContext<WarehouseDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb")));
@@ -14,6 +21,7 @@ builder.Services.AddDbContext<WarehouseDbContext>(options =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
 builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
+
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
 builder.Services.AddAutoMapper(cfg => { }, typeof(Program).Assembly);
@@ -28,6 +36,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseMiddleware<CorrelationIdMiddleware>();
+app.UseMiddleware<RequestTimingMiddleware>();
 
 app.MapControllers();
 
