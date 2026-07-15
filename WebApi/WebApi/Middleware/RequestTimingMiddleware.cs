e9728a9ceadf426f8980ef0504e4446d6247a5f9
FaresMaady <farsmaady@gmail.com>
2026-07-15 11:22:24 +0300

    feat(session-05): add request middleware

---
+++ WebApi/WebApi/Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi.Middleware;
+
+using System.Diagnostics;
+
+public class RequestTimingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestTimingMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers["X-Response-Time"] = $"{stopwatch.ElapsedMilliseconds}ms";
+            return Task.CompletedTask;
+        });
+
+        await _next(context);
+    }
+}
